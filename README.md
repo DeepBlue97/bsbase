8720efbfad15d60780d2cc61f67ad4f682d0cbf4
DeepBlue97 <945048933@qq.com>
2022-01-16 10:09:46 +0800

    django vue vite typescript

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bsbase
+
+## vite.config.js
+存在问题：.js文件运行不了
--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""bsbase URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from django.views.static import serve as serve_static
+#from backend.settings import STATICFILES_DIRS, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('/', TemplateView.as_view(template_name="index.html")),
+    #path('static/<path:path>', serve_static, {'document_root': STATIC_ROOT},),  # 处理静态文件
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     **url(r'^$', TemplateView.as_view(template_name="index.html")),**
+#     url(r'^api/', include('backend.urls', namespace='api'))
+# ]
